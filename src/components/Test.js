cb6f8928506656e29ce16eb83d9b260ccabca408
Nacer eddine <Nacereddinex220696@gmail.comm>
2022-08-08 13:44:27 +0100

    modified:   package-lock.json 	modified:   package.json 	modified:   public/favicon.ico 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg
     Untracked files:
    	postcss.config.js
    	src/Chat.js
    	src/Icon.svg
    	src/components/Board.js
    	src/components/Join.js
    	src/components/Square.js
    	src/tictactoe.js
    	tailwind.config.js

---
+++ src/components/Test.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function Test() {
+  return (
+    <div>
+                <div className="min-h-screen bg-gray-100 p-8">
+                     <div className="max-w-4xl mx-auto space-y-12 grid grid-cols-1">
+                        <div className="place-self-start">
+                        <div className="bg-white p-5 rounded-2xl rounded-tl-none">
+                            Hey, there! It's been a while!
+                        </div>
+                    </div>
+          
+                    <div className="place-self-end text-right">
+                        <div className="bg-green-50 text-green-900 p-5 rounded-2xl rounded-tr-none">
+                        Do you have any places in mind where you'd want to meet?
+                        </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Test
